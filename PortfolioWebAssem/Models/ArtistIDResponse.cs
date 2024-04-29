d7db4d0a087d46276c9ead7844a27d016e441c11
Dan <120772114+Elnubbio@users.noreply.github.com>
2024-04-29 06:50:16 +0100

    little update
    make reset button reset entered artist name + clear list of artists
    show list of songs being added to songlist so user can see something is happening
    fix issue where genius api returns a song title first rather than an artist name (see madonna by drake) - see if geniusapi fetch name matches genius api fetch result.artistname
    dark mode
    search by song title
    display last song during game
    fix not being able to click through right grid element on start page
    
    TODO - tidy up + merge functions

--- PortfolioWebAssem/Models/ArtistIDResponse.cs
+++ PortfolioWebAssem/Models/ArtistIDResponse.cs
@@ -18,11 +18,13 @@
 	public class Result
 	{
 		public PrimaryArtist primary_Artist { get; set; }
+		public string title { get; set; }
 	}
 
 	public class PrimaryArtist
 	{
 		public int id { get; set; }
+
 		//actually gonna use this name from now on to make sure matches line up \/
 		public string name { get; set; }
 	}
--- PortfolioWebAssem/Pages/MusicQuizPage/GeniusAPIClient.cs
+++ PortfolioWebAssem/Pages/MusicQuizPage/GeniusAPIClient.cs
@@ -10,6 +10,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using static System.Formats.Asn1.AsnWriter;
+using FuzzySharp;
 
 namespace PortfolioWebAssem.Pages.MusicQuizPage
 {
@@ -18,41 +19,94 @@ namespace PortfolioWebAssem.Pages.MusicQuizPage
 		private readonly string _apiKey = "RpBaUk-iRsnuJJ8ozLN7ZCjSQyd-MU4Vnp2F2HxOllHYPRpVRgooijV63QEToIdT";
 		public string NewArtistName = "";
 
-		public async Task<string> GetArtistId(string artistName)
+		public async Task<string> GetArtistIdUsingArtistName(string artistName)
 		//returns Genius API's Artist ID for a selected artist.
 		{
 			string _fetchURLForArtistID = $"https://api.genius.com/search?q={artistName}&access_token={_apiKey}";
 			HttpClient httpClient = new();
 			string artistID = "";
-
+			Console.WriteLine("inside function");
 			try { 
 			//fetch
 			var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, _fetchURLForArtistID);
 			var response = await httpClient.SendAsync(httpRequestMessage);
 			var returnObject = await response.Content.ReadFromJsonAsync<ArtistIDResponse>();
-
+				Console.WriteLine();
+			double artistSimilarity;
 			//check if this artist has any songs
-			if (returnObject.response.hits.Count > 0)
-			{
-				artistID = returnObject.response.hits.FirstOrDefault().result.primary_Artist.id.ToString();
-				NewArtistName = returnObject.response.hits.FirstOrDefault().result.primary_Artist.name.ToString();
-			}
+			//if (returnObject.response.hits.Count > 0)
+			//{
+			//	artistID = returnObject.response.hits.FirstOrDefault().result.primary_Artist.id.ToString();
+			//	NewArtistName = returnObject.response.hits.FirstOrDefault().result.primary_Artist.name.ToString();
+			//}
 
+			foreach (Hit hit in returnObject.response.hits)
+				{
+					artistSimilarity = Fuzz.Ratio(hit.result.primary_Artist.name, artistName) ;
+					//check if SELECTED artistName matches this artist name
+					Console.WriteLine($"Checking {hit.result.primary_Artist.name} against {artistName}, score: {artistSimilarity}");
+					if (artistSimilarity > 90)
+					{
+						//found match - 90 in case of different spacing etc.
+						artistID = hit.result.primary_Artist.id.ToString();
+						NewArtistName = hit.result.primary_Artist.name.ToString();
+						break;
+					}
+				}
 			} catch (Exception e)
 			{
-				Console.WriteLine($"Could not find artistID for {artistName}. Exception: ", e);
+				Console.WriteLine($"Could not find artistID for {artistName}. Exception: {e}");
 			}
 			return artistID;
 		}
 
-		public async Task<List<string>> GetSongTitles(string artistId)
-		//returns Genius API's Song Titles using Genius API's Artist ID. Returns 20 song titles - can be increased
+		public async Task<string> GetArtistIdUsingSongTitle(Recording recording)
+		//returns Genius API's Artist ID for a selected song title + artist combo.
 		{
-			string _fetchURLForSongs = $"https://api.genius.com/artists/{artistId}/songs?sort=popularity&access_token={_apiKey}";
+			string _fetchURLForArtistID = $"https://api.genius.com/search?q={recording.title}&access_token={_apiKey}";
+			HttpClient httpClient = new();
+			string artistID = "";
+
+			try
+			{
+				//fetch
+				var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, _fetchURLForArtistID);
+				var response = await httpClient.SendAsync(httpRequestMessage);
+				var returnObject = await response.Content.ReadFromJsonAsync<ArtistIDResponse>();
+				double songTitleSimilarity;
+
+				foreach (Hit hit in returnObject.response.hits)
+				{
+					songTitleSimilarity = Fuzz.Ratio(hit.result.primary_Artist.name.ToLower(), recording.artistcredit.First().name.ToLower());
+					//check if SELECTED artistName matches this song's artist name
+					Console.WriteLine($"Checking {hit.result.title} against {recording.title}, score: {songTitleSimilarity}");
+					if (songTitleSimilarity > 90)
+					{
+						//found match - 90 in case of different spacing etc.
+						artistID = hit.result.primary_Artist.id.ToString();
+						NewArtistName = hit.result.primary_Artist.name.ToString();
+						break;
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Could not find artistID for {recording.title}. Exception: ", e);
+			}
+			return artistID;
+		}
+
+		public async Task<List<string>> GetSongTitles(string artistId, int numberOfSongs)
+		//returns Genius API's Song Titles using Genius API's Artist ID. Returns numberOfSongs song titles - can be increased
+		{
 			List<Song> songsFromFetch = new();
 			List<string> songTitles = new();
 			HttpClient httpClient = new();
 
+			//base case
+			//numberOfSongs = 20;
+
+			string _fetchURLForSongs = $"https://api.genius.com/artists/{artistId}/songs?sort=popularity&per_page={numberOfSongs}&access_token={_apiKey}";
 			//sometimes the api returns an empty string for artistId since response.hits is empty - see "1StepKloser", so skip fetch
 			if (artistId != "")
 			{
@@ -65,17 +119,19 @@ namespace PortfolioWebAssem.Pages.MusicQuizPage
 
 					songsFromFetch = returnObject.response.songs;
 					//put song titles in my list
-					foreach (Song song in songsFromFetch)
-					{
-						songTitles.Add(song.title);
-					}
+					
+						foreach (Song song in songsFromFetch)
+						{
+							songTitles.Add(song.title);
+						}
+					
 				}
 				catch (Exception e)
 				{
 					Console.WriteLine($"Could not find song titles for {artistId}. Exception: ", e);
 				}
 			}
-			//20 song titles
+			//numberOfSongs song titles
 			return songTitles;
 		}
 		
